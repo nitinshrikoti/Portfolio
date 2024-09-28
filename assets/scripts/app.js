b5265734be73a0e5aa18e28af582a2ca4a12b1d1
nitinshrikoti1 <nitinshrikoti1@gmail.com>
2024-09-28 13:01:18 +0530

    Updated menu bar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,10 +4,12 @@ const navLinks = document.querySelector(".nav-links");
 menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   navLinks.classList.toggle("active");
+  navLinks.classList.toggle("hide");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   menu.classList.remove("active");
   navLinks.classList.remove("active");
+  navLinks.classList.remove("hide");
 
 }))
